44ccdb81c1237761ed368b607d43cb0ffd2598d6
postscriptum <dmitriy.semenovskiy@gmail.com>
2016-04-10 17:27:05 +0000

    Make some fields optional

--- catalog/models.py
+++ catalog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Gallery(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
     def number_of_images(self):
         return self.image_set.count()
@@ -15,8 +15,8 @@ class Gallery(models.Model):
 class Image(models.Model):
     file = models.ImageField(upload_to='images')
     name = models.CharField(max_length=200)
-    description = models.TextField()
-    galleries = models.ManyToManyField(Gallery)
+    description = models.TextField(blank=True)
+    galleries = models.ManyToManyField(Gallery, blank=True)
 
     def __str__(self):
         return self.name
