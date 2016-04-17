3ef7cde684cc7d01d882daa26df63d4e4e772bb2
postscriptum <dmitriy.semenovskiy@gmail.com>
2016-04-17 19:46:45 +0000

    App 'front': first steps

--- gallerycms/urls.py
+++ gallerycms/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from front import views as front_views
+
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', front_views.main, name='main'),
+    url(r'^control/', include(admin.site.urls)),
 ]
