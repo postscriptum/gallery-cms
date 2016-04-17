4b29872bffd2e3bf82826e49cc937515f3c41274
postscriptum <dmitriy.semenovskiy@gmail.com>
2016-04-17 21:36:09 +0000

    Create General model

--- front/models.py
+++ front/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class General(models.Model):
+    title = models.CharField(max_length=200)
+    under_construction = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "General site settings"
