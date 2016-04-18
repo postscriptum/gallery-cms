ea750c1732a71da7782b2cc32103b6efe022eb2a
postscriptum <dmitriy.semenovskiy@gmail.com>
2016-04-18 10:02:50 +0000

    Handle errors and under_construction status

--- front/views.py
+++ front/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import General
 
 
 def main(request):
-    return render(request, 'front/main.html')
+    if General.objects.count() > 0:
+        general = General.objects.all()[0]
+        if general.under_construction:
+            # UNDER CONSTRUCTION page
+            context = {'comment': 'Coming back soon'}
+            return render(request, 'front/under_construction.html', context)
+        # NORMAL page
+        context = {
+            'general': general
+        }
+        return render(request, 'front/main.html', context)
+    else:
+        # ERROR page
+        context = {'error_description': 'No general site settings'}
+        return render(request, 'front/error.html', context)
