980c8c176f469c59a866036fb06add0c1029474f
postscriptum <dmitriy.semenovskiy@gmail.com>
2016-04-22 20:45:39 +0000

    Connect handler to Image post_save signal

--- catalog/apps.py
+++ catalog/apps.py
@@ -1,8 +1,12 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
+from catalog.signals import make_preview
+from catalog.models import Image
 
 
 class CatalogConfig(AppConfig):
     name = 'catalog'
 
     def ready(self):
-        print("TEST: Catalog App is Ready!!!")  #test
+        # after image saving call 'make_preview' to make image preview
+        post_save.connect(make_preview, sender=Image)
---
+++ catalog/signals.py
@@ -0,0 +1,2 @@
+def make_preview(sender, **kwargs):
+    print("TEST: Signal is Received!!! Model: " + str(sender))  #test
