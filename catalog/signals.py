2ecb118fc8b175afed36b467f056715588687cba
postscriptum <dmitriy.semenovskiy@gmail.com>
2016-04-24 19:02:35 +0000

    Make square previews for uploaded images

--- catalog/signals.py
+++ catalog/signals.py
@@ -1,2 +1,27 @@
+import os.path
+from os import mkdir
+from PIL import Image
+
+
 def make_preview(sender, **kwargs):
-    print("TEST: Signal is Received!!! Model: " + str(sender))  #test
+    """Makes preview for uploaded image"""
+    image = kwargs['instance']
+    image_path = image.file.path
+    image_dir, image_filename = os.path.split(image_path)
+    previews_dir = os.path.join(image_dir, 'previews')
+    if not os.path.exists(previews_dir): mkdir(previews_dir)
+    preview_path = os.path.join(previews_dir, 'thumb_' + image_filename)
+    try:
+        img = Image.open(image_path)
+        width, height = img.size
+        # crop to square
+        if width > height:
+            left = (width - height) // 2
+            img.crop((left, 0, left + height, height)).save(preview_path)
+        elif height > width:
+            top = (height - width) // 2
+            img.crop((0, top, width, top + width)).save(preview_path)
+        else:
+            img.save(preview_path)
+    except:
+        pass
