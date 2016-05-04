b2488e4be043f064f873f6b8177b9645dee2d874
postscriptum <dmitriy.semenovskiy@gmail.com>
2016-05-04 14:41:01 +0000

    Bugfix in catalog app config

--- catalog/apps.py
+++ catalog/apps.py
@@ -1,14 +1,14 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_save, post_delete
 from catalog.signals import make_preview, delete_images
-from catalog.models import Image
 
 
 class CatalogConfig(AppConfig):
     name = 'catalog'
 
     def ready(self):
+        image_model = self.get_model('Image')
         # after image saving call 'make_preview' to make image preview
-        post_save.connect(make_preview, sender=Image)
+        post_save.connect(make_preview, sender=image_model)
         # after image deleting call 'delete_images' to delete unused image files
-        post_delete.connect(delete_images, sender=Image)
+        post_delete.connect(delete_images, sender=image_model)
