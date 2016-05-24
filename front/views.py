3f84c20cfff95059b7fec0e9138faa293076106b
postscriptum <dmitriy.semenovskiy@gmail.com>
2016-05-24 11:54:55 +0000

    Refactor templates

--- front/views.py
+++ front/views.py
@@ -1,39 +1,16 @@
 from django.shortcuts import render, get_object_or_404
-from front.models import General
 from catalog.models import Gallery, Article
 
 
 def main(request):
-    if General.objects.count() > 0:
-        general = General.objects.all()[0]
-        if general.under_construction:
-            # UNDER CONSTRUCTION page
-            context = {'comment': 'Coming back soon'}
-            return render(request, 'front/under_construction.html', context)
-        # NORMAL page
-        context = {
-            'galleries': Gallery.objects.filter(enabled=True)
-        }
-        return render(request, 'front/main.html', context)
-    else:
-        # ERROR page
-        context = {'error_description': 'No general site settings'}
-        return render(request, 'front/error.html', context)
+    context = {
+        'galleries': Gallery.objects.filter(enabled=True)
+    }
+    return render(request, 'front/main.html', context)
 
 
 def get_article(request, slug):
-    if General.objects.count() > 0:
-        general = General.objects.all()[0]
-        if general.under_construction:
-            # UNDER CONSTRUCTION page
-            context = {'comment': 'Coming back soon'}
-            return render(request, 'front/under_construction.html', context)
-        # NORMAL page
-        context = {
-            'article': get_object_or_404(Article, slug=slug)
-        }
-        return render(request, 'front/article.html', context)
-    else:
-        # ERROR page
-        context = {'error_description': 'No general site settings'}
-        return render(request, 'front/error.html', context)
+    context = {
+        'article': get_object_or_404(Article, slug=slug)
+    }
+    return render(request, 'front/article.html', context)
