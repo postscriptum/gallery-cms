88768cfd8da648341986d718f1910d56ef3bd81d
postscriptum <dmitriy.semenovskiy@gmail.com>
2016-05-25 10:03:33 +0000

    Add articles to context

--- common/context_processors.py
+++ common/context_processors.py
@@ -1,8 +1,11 @@
 from front.models import General
+from catalog.models import Article
 
 
 def general(request):
-    context = {}
+    context = {
+        'articles': Article.objects.filter(enabled=True)
+    }
     if General.objects.count() > 0:
         context['general'] = General.objects.all()[0]
     return context
